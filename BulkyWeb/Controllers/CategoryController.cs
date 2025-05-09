da4c0033df337b7e01baf1efe46d44cd6b234acd
Tushar <tusharsql@gmail.com>
2025-05-09 21:41:25 +0530

    09-May-2025

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            List<Category> objCategoryList = await _context.Categories.ToListAsync();
+            return View(objCategoryList);
+        }
+    }
+}
